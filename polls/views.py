bd524c9bba36fb1b0f15254ca407db1926f74a28
JoeSvr95 <joesaverio@hotmail.com>
2017-12-27 02:21:54 -0500

    Estilos

--- polls/views.py
+++ polls/views.py
@@ -4,7 +4,7 @@ from django.http import Http404
 from .models import Aeropuerto
 from .models import Aerolinea
 from .models import Ruta
-  
+
 def index(request):
 	origenes= []
 	all_aeropuerto = Aeropuerto.objects.all()
@@ -15,7 +15,7 @@ def index(request):
 	destino = origenes
 	contenido={"paises":origenes,}
 	return render (request,"formulario.html",contenido)
-	
+
 #Vista detallada de cada aeropuerto
 def detalle_aeropuerto(request,Airport_ID):
 	try:
@@ -23,24 +23,24 @@ def detalle_aeropuerto(request,Airport_ID):
 		contenido = contenido={"aeropuerto":aeropuerto,}
 	except Aeropuerto.DoesNotExist:
 		raise Http404("Aeropuerto no existe")
-		
+
 	return render(request,"modelo_aeropuerto.html",contenido)
-	
+
 #Vista General de todos los Aeropuertos
 def aeropuertos(request):
 	all_aeropuerto = Aeropuerto.objects.all()
 	contenido={"aeropuertos":all_aeropuerto,}
-	
+
 	return render (request,"aeropuertos.html",contenido)
-	
-#Vista General de todas las Aerolineas	
+
+#Vista General de todas las Aerolineas
 def aerolinea(request):
 	all_aerolinea = Aerolinea.objects.all()
 	contenido={"aerolineas":all_aerolinea,}
-	
+
 	return render(request,"aerolineas.html",contenido)
-	
-#Vista detallada de cada aerolinea	
+
+#Vista detallada de cada aerolinea
 def detalle_aerolinea(request,Airline_ID):
 	try:
 		aerolinea = Aerolinea.objects.get(Airline_ID = str(Airline_ID))
@@ -48,7 +48,7 @@ def detalle_aerolinea(request,Airline_ID):
 	except Aerolinea.DoesNotExist:
 		raise Http404("Aerolinea no existe")
 	return render(request,"modelo_aerolinea.html",contenido)
-	
+
 def search(request):
 	origenes= []
 	all_aeropuerto = Aeropuerto.objects.all()
@@ -57,12 +57,12 @@ def search(request):
 	origenes = list(set(origenes))
 	origenes.sort()
 	destino = origenes
-	contenido={"paises":origenes,}
-	
-	
+	contenido={"paises":origenes}
+
+
 	try:
 		origen= request.POST["origen"]
-		destino = request.POST["destino"] 
+		destino = request.POST["destino"]
 		bandera_con_escala = request.POST["vuelo"]
 		contenido["origen"]=origen
 		contenido["destino"]=destino
@@ -72,13 +72,11 @@ def search(request):
 			rutas = Ruta.objects.filter(Source_airport_ID__Country=origen,Destination_airport_ID__Country=destino)
 		respuesta=[]
 		for ruta in rutas:
-			try:					
+			try:
 				aerolinea = Aerolinea.objects.get(Airline_ID = ruta.Airline_ID)
-				if(aerolinea.Active == "Y"):				
+				if(aerolinea.Active == "Y"):
 					opcion = []
 					opcion.append(aerolinea.Name)
-					opcion.append(aerolinea.IATA)
-					opcion.append(ruta.Airline_ID)
 					opcion.append(ruta.Source_airport_ID.Name)
 					opcion.append(ruta.Destination_airport_ID.Name)
 					opcion.append(ruta.Stops)
@@ -89,6 +87,5 @@ def search(request):
 	except:
 		raise Http404("Problemas con el FORMULARIO")
 	return render (request,"formulario.html",contenido)
-		
-	
-	
+
+
