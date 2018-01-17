2568162cf20c840fb85b6c69f60cdc3c459e7bd2
Juan Crow Washbrum <jjcrow@espol.edu.ec>
2018-01-17 10:33:51 -0500

    agregando serializer user

--- polls/api/serializers.py
+++ polls/api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from ..models import Aerolinea, Aeropuerto, Ruta
+from ..models import Aerolinea, Aeropuerto, Ruta, Usuario
 
 class AerolineaSerializer(serializers.ModelSerializer):
     class Meta:
@@ -17,3 +17,9 @@ class RutaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Ruta
         fields = '__all__'
+	
+class UsuarioSerializer(serializers.HyperlinkedModelSerializer):
+	class Meta:
+		model= Usuario
+		fields=('nombre','celular','f_partida','f_retorno','ciudad_partida','ciudad_llegada','escala')
+	
